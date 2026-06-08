75e37d8f77218579fea90c43fe300254f3289427
li <li@goldragon.criome.net>
2026-06-08 02:16:46 +0200

    (("Cleanup", "target"), ("Remove", "tracked build artifacts"), ("Verdict", "target is ignored generated output"))

--- target/debug/deps/subtle-25cc9b31ed371902.d
+++
@@ -1,7 +0,0 @@
-/home/li/git/clavifaber/target/debug/deps/subtle-25cc9b31ed371902.d: /home/li/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/subtle-2.6.1/src/lib.rs
-
-/home/li/git/clavifaber/target/debug/deps/libsubtle-25cc9b31ed371902.rlib: /home/li/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/subtle-2.6.1/src/lib.rs
-
-/home/li/git/clavifaber/target/debug/deps/libsubtle-25cc9b31ed371902.rmeta: /home/li/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/subtle-2.6.1/src/lib.rs
-
-/home/li/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/subtle-2.6.1/src/lib.rs:
